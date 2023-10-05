c8832f296691f78bd6768b6e72bdb2bc99297184
freebie22 <bojkov755@gmail.com>
2023-10-05 16:49:53 +0300

    Added first HTTP Endpoint(GET)

---
+++ Magic Villa_VillaApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,16 @@
+﻿using Magic_Villa_VillaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magic_Villa_VillaApi.Controllers
+{
+    [ApiController]
+    [Route("api/VillaAPI")]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new Villa[] { new Villa() { ID = 1, Name = "Pool Villa" }, new Villa() { ID = 1, Name = "Mountain Villa" } };   
+        }
+    }
+}
---
+++ Magic Villa_VillaApi/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace Magic_Villa_VillaApi.Models
+{
+    public class Villa
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
