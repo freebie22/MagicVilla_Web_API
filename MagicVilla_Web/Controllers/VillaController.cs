15ab3b0b307dcb5835b0d8aabc94f9e6f8261626
freebie22 <bojkov755@gmail.com>
2023-11-03 13:22:04 +0200

    CRUD operations with Villa is ready

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -44,10 +44,80 @@ namespace MagicVilla_Web.Controllers
             if(response != null && response.IsSuccess == true)
             {
                 return RedirectToAction(nameof(Index));
-            }
+            } 
+
             return View(villaCreateDTO);
             
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if(id == 0)
+            {
+                return View(nameof(Index));
+            }
+
+            VillaDTO villa = new();
+
+            var response = await _service.GetAsync<APIResponse>(id);
+
+            if(response != null && response.IsSuccess)
+            {
+                villa = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+            }
+
+            return View(villa);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(VillaDTO villaDTO)
+        {
+            if(villaDTO != null)
+            {
+                var response = await _service.DeleteAsync<APIResponse>(villaDTO.Id);
+
+                if(response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(villaDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id == 0)
+            {
+                return View(nameof(Index));
+            }
+
+            var villaDTO = new VillaUpdateDTO();
+
+            var responseToUpdate = await _service.GetAsync<APIResponse>(id);
+
+            if(responseToUpdate != null && responseToUpdate.IsSuccess)
+            {
+                villaDTO = JsonConvert.DeserializeObject<VillaUpdateDTO>(Convert.ToString(responseToUpdate.Result));
+            }
+
+            return View(villaDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(VillaUpdateDTO villaDTO)
+        {
+            if (villaDTO != null)
+            {
+                var response = await _service.UpdateAsync<APIResponse>(villaDTO);
+
+                if(response != null && response.IsSuccess == true)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(villaDTO);
+        }
     }
 }
